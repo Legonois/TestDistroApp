ec96e1a2c251fcb66d9b4f113fe830a86e309d6f
Legonois <wesley.baldwin@outlook.com>
2022-04-24 15:29:11 -0500

    Test Website

---
+++ MyFirstAzureWebApp/Pages/Resources.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstAzureWebApp.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
